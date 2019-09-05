917e932339586ae7fe16da7514cc39e06baf800f
Victor Flores <vflorespineda@gmail.com>
2019-09-05 12:37:10 -0500

    Graph of distribution and byteStuffing function

--- simTramas.py
+++ simTramas.py
@@ -4,22 +4,43 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-
+#Genera las tramas de longitud aleatoria con bits aleatorios
 def generarTramas():
     tramas = list()
-    rand = np.random.randint(5,11,1000)
+    rand = np.random.randint(5,11,10000)
     for i in range(0,999):
-        tramas.append(np.random.bytes(rand[i]))
-    distTramas(rand)    
+        tramas.append(list(np.random.bytes(rand[i])))
+    distTramas(rand)
+    return tramas    
 
+#Calcula la distribución de las tramas y muestra una gráfica
 def distTramas(rand):
         count = np.zeros(6)
         for j in rand:
                 index = j-5
-                count[index] = count[index]+1  
-        print(count)
+                count[index] = count[index]+1
+        xAxis = np.linspace(5,10,6)  
+        plt.stem(xAxis,count)
+        plt.ylim(0,1900)
+        plt.title('Distribución de longitud de tramas')
+        plt.show()
+        
+#Se genera el byte stuffing
+def byteStuffing(tramas):
+        byteIni = 245   #representacion ascii de (§)
+        byteFin = 208     #representacion ascii de (ð)
+        for trama in tramas:
+                trama.insert(0,byteIni)
+                trama.append(byteFin)   
 
-generarTramas()
+#Genera un cierto porcentaje de error en las tramas
+def errPorcen(porcentaje,tramas):
+        cantErrores = tramas*porcentaje
+        
+
+
+tramas = generarTramas()
+byteStuffing(tramas)
 
 
 
