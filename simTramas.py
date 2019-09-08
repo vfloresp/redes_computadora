7440f57149063e580a6d73f169ec1f3d90201317
Victor desktop <vflorespineda@gmail.com>
2019-09-08 15:18:42 -0500

    byte stuffing
    Funcion de byte stuffing y gráficas de distribución listas

--- simTramas.py
+++ simTramas.py
@@ -8,23 +8,32 @@ import matplotlib.pyplot as plt
 def generarTramas():
     tramas = list()
     rand = np.random.randint(5,11,10000)
-    for i in range(0,999):
+    for i in range(0,9999):
         tramas.append(list(np.random.bytes(rand[i])))
-    distTramas(rand)
     return tramas    
 
+
 #Calcula la distribución de las tramas y muestra una gráfica
-def distTramas(rand):
-        count = np.zeros(6)
-        for j in rand:
-                index = j-5
+def longTramas(tramas):
+        count = np.zeros(15)
+        max = 0
+        for trama in tramas:
+                if len(trama)>max:
+                        max = len(trama)
+                index = len(trama)
                 count[index] = count[index]+1
-        xAxis = np.linspace(5,10,6)  
-        plt.stem(xAxis,count)
+        min = 0
+        for n in count:
+                if n != 0:
+                        break
+                min = min +1
+        xAxis = np.linspace(min,max,max-min+1)
+        yAxis = count[min:max+1]
+        plt.figure()
+        plt.stem(xAxis,yAxis)
         plt.ylim(0,1900)
         plt.title('Distribución de longitud de tramas')
-        plt.show()
-        
+
 #Se genera el byte stuffing
 def byteStuffing(tramas):
         byteIni = 245     #representacion ascii de (§)
@@ -32,10 +41,13 @@ def byteStuffing(tramas):
         esc = 27         #representación ascii de ESC
         for trama in tramas:
                 aux = 0
-                for b in trama:
+                tramaIter = iter(trama)
+                for b in tramaIter:
                         if b == byteIni or b == byteFin or b == esc :
                                 trama.insert(aux,esc)
                                 aux = aux + 1
+                                b = next(tramaIter)
+                        aux + aux+1
                 trama.insert(0,byteIni)
                 trama.append(byteFin)
         return tramas   
@@ -47,7 +59,14 @@ def errPorcen(porcentaje,tramas):
 
 
 tramas = generarTramas()
+print(len(tramas))
+longTramas(tramas)
 tramasStuffing = byteStuffing(tramas)
-
+longTramas(tramasStuffing)
+tramas2 = errPorcen(0.2,tramasStuffing)
+tramas1 = errPorcen(0.1,tramasStuffing)
+tramas05 = errPorcen(0.05,tramasStuffing)
+tramas02 = errPorcen(0.02,tramasStuffing)
+plt.show()
 
 
