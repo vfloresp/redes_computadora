c4e362f24aa6d55d63a649a127905ebb09f28fb9
Victor Flores <vflorespineda@gmail.com>
2019-10-08 13:30:57 -0500

    segunda prueba

--- terminalA.py
+++ terminalA.py
@@ -2,16 +2,16 @@ import serial
 import time
 import numpy as numpy
 
-s = serial.Serial('/dev/USB-SERIAL CH340')
+s = serial.Serial('COM5')
 
 s.is_open
 n = 0
-while n<1000:
-    slotToSend = np.randInt(1,5)
+while n<500:
+    slotToSend = np.random.randint(1,5)
     time.sleep(slotToSend/1000*50)
-    s.write(b'TA:')
-    random = np.randInt(0,256)
-    s.write(random.to_bytes(1,byteorder="little"))
+    s.write('TA:')
+    random = np.random.randint(0,256)
+    s.write(random)
     n = n+1
 
 s.close()
