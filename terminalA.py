e680c1e7f1607b03752b67ef4cdc0a1ff13d0ca7
Victor Flores <vflorespineda@gmail.com>
2019-10-08 13:34:14 -0500

    tercer prueba

--- terminalA.py
+++ terminalA.py
@@ -1,6 +1,6 @@
 import serial 
 import time
-import numpy as numpy
+import numpy as np
 
 s = serial.Serial('COM5')
 
@@ -9,9 +9,10 @@ n = 0
 while n<500:
     slotToSend = np.random.randint(1,5)
     time.sleep(slotToSend/1000*50)
-    s.write('TA:')
+    s.write(b'TA:')
     random = np.random.randint(0,256)
-    s.write(random)
+    s.write(random.to_bytes(1,byteorder="little"))
+    a.write(b'\n')
     n = n+1
 
 s.close()
