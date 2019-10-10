53584b02b4e4cfb770a71b187c8fa41440b1c120
Victor Flores <vflorespineda@gmail.com>
2019-10-10 16:46:08 -0500

    tres terminales

--- terminalB.py
+++ terminalB.py
@@ -6,10 +6,10 @@ s = serial.Serial('COM5')
 
 s.is_open
 n = 0
-while n<500:
+while n<600:
     slotToSend = np.random.randint(1,5)
     time.sleep(slotToSend/1000*100)
-    s.write(b'TA:')
+    s.write(b'TB:')
     random = np.random.randint(0,256)
     s.write(random.to_bytes(1,byteorder="little"))
     s.write(b'\n')
---
+++ terminalC.py
@@ -0,0 +1,15 @@
+import serial 
+import time
+import numpy as np
+
+s = serial.Serial('COM5')
+
+s.is_open
+
+while 1:
+  tdata = s.read()           # Wait forever for anything
+  time.sleep(1)              # Sleep (or inWaiting() doesn't give the correct value)
+  data_left = s.inWaiting()  # Get the number of characters ready to be read
+  tdata += s.read(data_left) # Do the read and combine it with the first character
+
+s.close()
