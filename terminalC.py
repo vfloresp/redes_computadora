ccdf1d1f4214603faeffae7cf6b233dda654840f
Victor Flores <vflorespineda@gmail.com>
2019-10-11 12:13:53 -0500

    intento de lectura

--- terminalC.py
+++ terminalC.py
@@ -7,9 +7,10 @@ s = serial.Serial('COM5')
 s.is_open
 
 while 1:
-  tdata = s.read()           # Wait forever for anything
+  data = s.read()           # Wait forever for anything
+  print(data.decode(7))
   time.sleep(1)              # Sleep (or inWaiting() doesn't give the correct value)
-  data_left = s.inWaiting()  # Get the number of characters ready to be read
-  tdata += s.read(data_left) # Do the read and combine it with the first character
+  #data_left = s.inWaiting()  # Get the number of characters ready to be read
+  #tdata += s.read(data_left) # Do the read and combine it with the first character
 
 s.close()
