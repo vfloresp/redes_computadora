ca2afbc2863732fb7fd6fbf210870c68940a6e09
Victor Flores <vflorespineda@gmail.com>
2019-10-11 12:00:11 -0500

    versión que imprime los envios

--- terminalB.py
+++ terminalB.py
@@ -10,8 +10,8 @@ while n<600:
     slotToSend = np.random.randint(1,5)
     time.sleep(slotToSend/1000*100)
     s.write(b'TB:')
-    random = np.random.randint(0,256)
-    s.write(random.to_bytes(1,byteorder="little"))
+    random = str(np.random.randint(0,256))
+    s.write(random.encode())
     s.write(b'\n')
     n = n+1
 
