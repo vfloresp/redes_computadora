343ff2291f61369524a1dbd6a96d1993052ef94c
Victor Flores <vflorespineda@gmail.com>
2019-10-14 10:53:07 -0500

    deteccion colisiones

--- terminalA.py
+++ terminalA.py
@@ -4,6 +4,9 @@ import numpy as np
 
 s = serial.Serial('COM5')
 
+envios = 0
+colisiones = 0
+reenvio = 0
 s.is_open
 n = 0
 while n<500:
@@ -17,11 +20,15 @@ while n<500:
     time.sleep(slotToSend/1000*55)
     data = s.read_until(size=7)
     try:
-       dataS = data.decode()
-       source = dataS[:2]
-       if source == "TC":
-           print(dataS)
+        dataS = data.decode()
+        source = dataS[:3]
+        val = int(dataS[3:])
+        if(source!="TC" or source!="TA" or val<0 or val>255):
+            colisiones = colisiones + 1
+        elif source == "TC":
+            print(dataS)
     except:
        print("Colision")
+       colisiones = colisiones +1
 
 s.close()
