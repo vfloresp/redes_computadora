f2a68a981de6c7dd68ff00cf11cc08d24e869073
Victor Flores <vflorespineda@gmail.com>
2019-11-13 11:32:38 -0600

    tarea Nat

--- client.py
+++ client.py
@@ -2,12 +2,16 @@
 
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+data = ''
+HOST = '10.2.73.251'  # The server's hostname or IP address
+PORT = 6001        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'Hello, world')
-    data = s.recv(1024)
+    try:
+        data = s.recv(1024)
+    except socket.error as ex:
+        print(ex)
 
 print('Received', repr(data))
